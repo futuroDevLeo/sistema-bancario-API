9402c2fbb8c8bfd25609fe2f63023c83226ab81b
futuroDevLeo <leonunes07@outlook.com>
2023-09-08 20:59:47 -0300

    remoção de dados exemplo do bancodedados.js

--- src/data/bancodedados.js
+++ src/data/bancodedados.js
@@ -5,32 +5,7 @@ module.exports = {
         agencia: '0001',
         senha: 'Cubos123Bank'
     },
-    contas: [
-        {
-            numero: "1",
-            saldo: 200,
-            usuario: {
-                nome: "Foo Bar",
-                cpf: "00011122234",
-                data_nascimento: "2021-03-15",
-                telefone: "71999998888",
-                email: "foo@bar.com",
-                senha: "12345"
-            }
-        },
-        {
-            numero: "2",
-            saldo: 1000,
-            usuario: {
-                nome: "Foo Bar 2",
-                cpf: "11111122234",
-                data_nascimento: "2021-03-15",
-                telefone: "73999998888",
-                email: "foo@bar2.com",
-                senha: "12345"
-            }
-        }
-    ],
+    contas: [],
     saques: [],
     depositos: [],
     transferencias: []
