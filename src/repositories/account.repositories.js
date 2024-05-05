5e58cf28e8b62b13d8fc20cbacf2713add66eeca
futuroDevLeo <leonunes07@outlook.com>
2024-05-05 00:51:06 -0300

    Revert "feat(REPOSITORIES): create a new layer of repositories containing functions used in services"
    This reverts commit 2bfc3bbeb5bf348ae4b49c0e3292a35d9b31b686.

--- src/repositories/account.repositories.js
+++
@@ -1,8 +0,0 @@
-import bancodedados from '../database/bancodedados.js';
-
-function findAccount(accountNumber) {
-    const contaExiste = bancodedados.contas.find(conta => conta.numero === accountNumber);
-    return contaExiste;
-}
-
-export default findAccount;
