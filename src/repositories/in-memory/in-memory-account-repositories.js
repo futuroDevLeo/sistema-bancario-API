cf094660b4316bd5d55f31a5938dd4f25845a285
futuroDevLeo <leonunes07@outlook.com>
2024-05-21 01:39:33 -0300

    test: add tests for update and delete account services
    - Implement tests for updateUserService
    - Implement tests for deleteAccountService
    - Add functions in inMemoryRepository for findByAccountNumber, deleteAccount, and updateUser
    - Ensure all tests pass and verify correct behavior

--- src/repositories/in-memory/in-memory-account-repositories.js
+++ src/repositories/in-memory/in-memory-account-repositories.js
@@ -8,10 +8,34 @@ const findByEmail = (email) => db.filter(account => account.user.email === email
 
 const findByCpf = (cpf) => db.filter(account => account.user.cpf === cpf);
 
+const findByAccountNumber = (accountNumber) => db.filter(account => account.accountNumber === accountNumber);
+
+const deleteAccount = (accountObject) => {
+    const index = db.findIndex(account => account.accountNumber === accountObject.accountNumber);
+    if (index !== -1) {
+        db.splice(index, 1);
+        return true;
+    }
+    return false;
+};
+
+const updateUser = (userObject) => {
+    const { accountNumber, user } = userObject;
+    const account = db.find(account => account.accountNumber === accountNumber);
+    if (account) {
+        account.user = user;
+        return true;
+    }
+    return false;
+};
+
 export default {
     db,
     findAll,
     createAccount,
     findByEmail,
-    findByCpf
+    findByCpf,
+    findByAccountNumber,
+    deleteAccount,
+    updateUser
 }
--- src/services/account.service.spec.js
+++ src/services/account.service.spec.js
@@ -23,7 +23,7 @@ describe('Account Services', () => {
                     birthdate: "2000-01-01",
                     phonenumber: "122333-9999",
                     email: "test@example.com",
-                    password: "teste_senha"
+                    password: "test_password"
                 }
             })
             onTestFinished(() => inMemoryRepository.db.pop());
@@ -45,7 +45,7 @@ describe('Account Services', () => {
                     birthdate: "2000-01-01",
                     phonenumber: "122333-9999",
                     email: "test@example.com",
-                    password: "teste_senha"
+                    password: "test_password"
                 }
             });
             const newAccount = {
@@ -54,7 +54,7 @@ describe('Account Services', () => {
                 birthdate: "1990-01-01",
                 phonenumber: "999888-7777",
                 email: "test@example.com", // Duplicate email
-                password: "teste_senha2"
+                password: "test_password2"
             };
             onTestFinished(() => inMemoryRepository.db.pop());
             try {
@@ -75,7 +75,7 @@ describe('Account Services', () => {
                     birthdate: "2000-01-01",
                     phonenumber: "122333-9999",
                     email: "test@example.com",
-                    password: "teste_senha"
+                    password: "test_password"
                 }
             });
             const newAccount = {
@@ -84,7 +84,7 @@ describe('Account Services', () => {
                 birthdate: "1990-01-01",
                 phonenumber: "999888-7777",
                 email: "jane@example.com",
-                password: "teste_senha2"
+                password: "test_password2"
             };
             onTestFinished(() => inMemoryRepository.db.pop());
             try {
@@ -101,13 +101,242 @@ describe('Account Services', () => {
                 birthdate: "1990-01-01",
                 phonenumber: "999888-7777",
                 email: "jane@example.com",
-                password: "teste_senha2"
+                password: "test_password2"
             };
             onTestFinished(() => inMemoryRepository.db.pop());
             await accountServices.createAccountService(newAccount, inMemoryRepository);
             const allAccounts = inMemoryRepository.db;
             expect(allAccounts.length).toBe(1);
             expect(allAccounts[0].user.name).toBe("Jane Doe");
+        });
+    });
+
+    describe('updateUserService', () => {
+        it('should throw an error if bank account not found', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "test@example.com",
+                    password: "test_password"
+                }
+            });
+            const updateAccount = {
+                name: "John Doe",
+                cpf: "00112233445",
+                birthdate: "2000-01-01",
+                phonenumber: "122333-9999",
+                email: "john@example.com",
+                password: "test_password"
+            };
+            const accountNumber = '2';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            try {
+                await accountServices.updateUserService(updateAccount, accountNumber, inMemoryRepository);
+            } catch (e) {
+                expect(e.message).toBe('Bank Account not found.');
+            }
+            const allAccounts = inMemoryRepository.db;
+            expect(allAccounts.length).toBe(1);
+            expect(allAccounts[0].user.email).toBe("test@example.com");
+        });
+        it('should throw an error if CPF is already in use and not update', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "test@example.com",
+                    password: "test_password"
+                }
+            });
+            inMemoryRepository.db.push({
+                accountNumber: '2',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "Jane Doe",
+                    cpf: "11223344556",
+                    birthdate: "1990-01-01",
+                    phonenumber: "999888-7777",
+                    email: "jane@example.com",
+                    password: "test_password"
+                }
+            });
+            const updateAccount = {
+                name: "Jane Doe",
+                cpf: "00112233445", // Duplicate CPF
+                birthdate: "1990-01-01",
+                phonenumber: "999888-7777",
+                email: "jane@test.com",
+                password: "test_password2"
+            };
+            const accountNumber = '2';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            onTestFinished(() => inMemoryRepository.db.pop());
+            try {
+                await accountServices.updateUserService(updateAccount, accountNumber, inMemoryRepository);
+            } catch (e) {
+                expect(e.message).toBe('An account already exists with the CPF or email provided.');
+            }
+            const allAccounts = inMemoryRepository.db;
+            expect(allAccounts.length).toBe(2);
+            expect(allAccounts[1].user.email).toBe("jane@example.com");
+            expect(allAccounts[1].user.password).toBe("test_password");
+        });
+        it('should throw an error if email is already in use and not update', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "test@example.com",
+                    password: "test_password"
+                }
+            });
+            inMemoryRepository.db.push({
+                accountNumber: '2',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "Jane Doe",
+                    cpf: "11223344556",
+                    birthdate: "1990-01-01",
+                    phonenumber: "999888-7777",
+                    email: "jane@example.com",
+                    password: "test_password"
+                }
+            });
+            const updateAccount = {
+                name: "Jane Doe",
+                cpf: "11223344556",
+                birthdate: "1990-01-01",
+                phonenumber: "999888-7777",
+                email: "test@example.com", // Duplicate email
+                password: "test_password2"
+            };
+            const accountNumber = '2';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            onTestFinished(() => inMemoryRepository.db.pop());
+            try {
+                await accountServices.updateUserService(updateAccount, accountNumber, inMemoryRepository);
+            } catch (e) {
+                expect(e.message).toBe('An account already exists with the CPF or email provided.');
+            }
+            const allAccounts = inMemoryRepository.db;
+            expect(allAccounts.length).toBe(2);
+            expect(allAccounts[1].user.email).toBe("jane@example.com");
+            expect(allAccounts[1].user.password).toBe("test_password");
+        });
+        it('should update an account successfully with valid data', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "test@example.com",
+                    password: "test_password"
+                }
+            });
+            inMemoryRepository.db.push({
+                accountNumber: '2',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "Jane Doe",
+                    cpf: "11223344556",
+                    birthdate: "1990-01-01",
+                    phonenumber: "999888-7777",
+                    email: "jane@example.com",
+                    password: "test_password"
+                }
+            });
+            const updateAccount = {
+                name: "Jane Doe",
+                cpf: "11223344556",
+                birthdate: "1990-01-01",
+                phonenumber: "999888-7777",
+                email: "jane@test.com",
+                password: "test_password2"
+            };
+            const accountNumber = '2';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            onTestFinished(() => inMemoryRepository.db.pop());
+            await accountServices.updateUserService(updateAccount, accountNumber, inMemoryRepository);
+            const allAccounts = inMemoryRepository.db;
+            expect(allAccounts.length).toBe(2);
+            expect(allAccounts[1].user.email).toBe("jane@test.com");
+            expect(allAccounts[1].user.password).toBe("test_password2");
+        });
+    });
+
+    describe('deleteAccountService', () => {
+        it('should throw an error if bank account not found', async () => {
+            const accountNumber = '1';
+            try {
+                await accountServices.deleteAccountService(accountNumber, inMemoryRepository);
+            } catch (e) {
+                expect(e.message).toBe('Bank Account not found.');
+            }
+        });
+        it('should throw an error if balance is greater than zero', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 100,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "john@example.com",
+                    password: "test_password"
+                }
+            });
+            const accountNumber = '1';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            try {
+                await accountServices.deleteAccountService(accountNumber, inMemoryRepository);
+            } catch (e) {
+                expect(e.message).toBe('Balance greater than zero.');
+            }
+        });
+        it('should delete the account successfully if balance is zero', async () => {
+            inMemoryRepository.db.push({
+                accountNumber: '1',
+                balance: 0,
+                createdAt: new Date(),
+                user: {
+                    name: "John Doe",
+                    cpf: "00112233445",
+                    birthdate: "2000-01-01",
+                    phonenumber: "122333-9999",
+                    email: "john@example.com",
+                    password: "test_password"
+                }
+            });
+            const accountNumber = '1';
+            onTestFinished(() => inMemoryRepository.db.pop());
+            await accountServices.deleteAccountService(accountNumber, inMemoryRepository);
+            const allAccounts = inMemoryRepository.db;
+            expect(allAccounts.length).toBe(0);
         });
     });
 });
